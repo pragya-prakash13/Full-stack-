8be2742635812cfb3ee272de5438057730e6fea1
pragya <prakash.pragya13@gmail.com>
2025-06-22 15:58:41 +0530

    Prep backend & serve frontend for Render

--- backend/server.js
+++ backend/server.js
@@ -22,4 +22,5 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use("/api", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
